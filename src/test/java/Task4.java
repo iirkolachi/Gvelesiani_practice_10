81629d28dd306982bbf6d9b8ee3432649730fe0b
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-11-07 23:20:56 +0400

    task_4_Tinatin

--- src/test/java/Task4.java
+++ src/test/java/Task4.java
@@ -5,6 +5,12 @@ public class Task4 {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("dog", "cat", "elephant", "tiger", "lion", "ant");
 
-        List<String> filteredWords = words.stream().filter(length -> length.length() > 4)
+        List<String> filteredWords = words.stream()
+                                     .filter(length -> length.length() < 4)
+                                     .map(String::toUpperCase) //(item -> item.toUpperCase())
+                                     .sorted()
+                                     .toList();
+
+        filteredWords.forEach(System.out::println); //(item -> System.out.println(item))
     }
 }
