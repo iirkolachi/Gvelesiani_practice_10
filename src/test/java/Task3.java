3daf664d4080229e436bfc311bab1e18715e88fc
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-11-07 23:08:08 +0400

    task_3_Tinatin

---
+++ src/test/java/Task3.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Task3 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Eve", "Charlie", "Daniel");
+
+        List<String> sortedNames = names.stream().sorted().toList();
+
+        int i = 0;
+
+        while (i < names.size()) {
+            System.out.println(sortedNames.get(i));
+            i++;
+        }
+    }
+}
